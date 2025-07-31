ac18ff37654f07e8746b58f7a52df97cc6ef8eb4
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-07-31 10:04:09 +0530

    create another event handler function

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,20 +1,37 @@
 import { useState } from "react";
 
 export default function Form() {
-  let [fullName, setFullName] = useState("Shreya");
+  let [fullName, setFullName] = useState("");
+  let [username, setuserName] = useState("");
 
   let handleChange = (event) => {
     setFullName(event.target.value);
   };
 
+  let handleUsername = (event) => {
+    setuserName(event.target.value);
+  };
+
   return (
     <form>
       <h2>My Form</h2>
+      <label htmlFor="fullName">FullName</label>
       <input
         type="text"
         placeholder="enter your name "
         value={fullName}
         onChange={handleChange}
+        id="fullName"
+      />
+      <br />
+      <br />
+      <label htmlFor="username">UserName</label>
+      <input
+        type="text"
+        placeholder="enter Username"
+        value={username}
+        onChange={handleUsername}
+        id="username"
       />
       <button>Submit</button>
     </form>
