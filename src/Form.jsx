d50861fecd6f9cc8c17e00023c27073f86a40d30
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-01 13:43:30 +0530

    Added dynamic input handler and submit logic with form reset

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,27 +1,46 @@
 import { useState } from "react";
 
 export default function Form() {
-  let [fullName, setFullName] = useState("");
-  let [username, setuserName] = useState("");
+  let [formData, setFormData] = useState({
+    fullName: "",
+    username: "",
+  });
 
-  let handleChange = (event) => {
-    setFullName(event.target.value);
+  //   let handleChange = (event) => {
+  //     setFullName(event.target.value);
+  //   };
+
+  //   let handleUsername = (event) => {
+  //     setuserName(event.target.value);
+  //   };
+
+  let handleInputChange = (event) => {
+    setFormData((currData) => {
+      return { ...currData, [event.target.name]: event.target.value };
+    });
   };
 
-  let handleUsername = (event) => {
-    setuserName(event.target.value);
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form submitted with data:", formData);
+
+    setFormData({
+      fullName: "",
+      username: "",
+    });
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>My Form</h2>
       <label htmlFor="fullName">FullName</label>
       <input
         type="text"
         placeholder="enter your name "
-        value={fullName}
-        onChange={handleChange}
+        onChange={handleInputChange}
+        value={formData.fullName}
         id="fullName"
+        name="fullName"
       />
       <br />
       <br />
@@ -29,9 +48,10 @@ export default function Form() {
       <input
         type="text"
         placeholder="enter Username"
-        value={username}
-        onChange={handleUsername}
+        onChange={handleInputChange}
+        value={formData.username}
         id="username"
+        name="username"
       />
       <button>Submit</button>
     </form>
