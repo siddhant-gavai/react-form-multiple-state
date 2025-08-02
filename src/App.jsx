ade62f5c77d2fd16b3fd12b0c55c1decbbea4641
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-08-02 10:26:32 +0530

    practice for form concepts

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Form from "./Form";
+import CommentForm from "./CommentForm";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Form />
+      <CommentForm />
     </>
   );
 }
---
+++ src/CommentForm.jsx
@@ -0,0 +1,9 @@
+export default function CommentForm() {
+  let [commentData, setCommentData] = useState({ comment: "" });
+
+  return (
+    <div>
+      <h4>Give a commment!</h4>
+    </div>
+  );
+}
